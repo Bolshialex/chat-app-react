10195b82b1b9593efdc27a3782c865a08714cdb4
Bolshialex <Bolshialex000@gmail.com>
2024-09-23 13:56:39 -0700

    Commit to change systems

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,20 @@
 import { useState } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Main from "./pages/Main";
+import Register from "./pages/Register";
 
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <Router>
-        <Route path="/register" component={Register} />
-        <Route path="/login" component={Login} />
-        <Route path="/" component={Main} />
-      </Router>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Main />} />
+      </Routes>
+    </Router>
   );
 }
 
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -4,7 +4,23 @@ import "../styles/global.css";
 function Login() {
   return (
     <>
-      <div className="card-container"></div>
+      <div className="card-container">
+        <h1>Login</h1>
+        <form>
+          <div>
+            <label>Username</label>
+            <input type="text" placeholder="Username" required />
+            <label>Password</label>
+            <input type="password" placeholder="Password" required />
+            <button type="submit">Login</button>
+          </div>
+          <div>
+            <p>
+              Don't have an account? <a href="/register">Register</a>
+            </p>
+          </div>
+        </form>
+      </div>
     </>
   );
 }
